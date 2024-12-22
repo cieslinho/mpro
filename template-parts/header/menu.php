38a1914971f98e6ab5f5f66e5e88e6bf063c0d0b
Szymon Cieśla <cszymon01@gmail.com>
2024-12-23 00:01:02 +0100

    global fixes

--- template-parts/header/menu.php
+++ template-parts/header/menu.php
@@ -1,3 +1,10 @@
+<?php
+$nav_boxes = get_field('nav_box', 'option');
+
+
+
+?>
+
 <?php $logo = get_theme_mod('custom_logo'); ?>
 <?php $logoMobile = get_theme_mod('custom_logo-mobile'); ?>
 <nav class="nav">
@@ -57,7 +64,7 @@
                                     d="M12 2C7.589 2 4 5.589 4 9.995 3.971 16.44 11.696 21.784 12 22c0 0 8.029-5.56 8-12 0-4.411-3.589-8-8-8zm0 12c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z">
                                 </path>
                             </svg>
-                            ul. Skromna 1, 86-005
+                            Plac Wolności 13, 16-075 Zawady
                         </p>
                         <p>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -66,7 +73,7 @@
                                     d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z">
                                 </path>
                             </svg>
-                            <a href="tel:+48537895760">+48 537 895 760</a>
+                            <a href="tel:+48519833388">+48 519 833 388</a>
                         </p>
                     </div>
                     <div class="nav__top-right">
@@ -101,40 +108,30 @@
         </div>
         <div class="nav__bottom">
             <div class="container">
-                <div class="nav__bottom-boxes">
-
-
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            style="transform: ;msFilter:;">
-                            <path
-                                d="M22 8a.76.76 0 0 0 0-.21v-.08a.77.77 0 0 0-.07-.16.35.35 0 0 0-.05-.08l-.1-.13-.08-.06-.12-.09-9-5a1 1 0 0 0-1 0l-9 5-.09.07-.11.08a.41.41 0 0 0-.07.11.39.39 0 0 0-.08.1.59.59 0 0 0-.06.14.3.3 0 0 0 0 .1A.76.76 0 0 0 2 8v8a1 1 0 0 0 .52.87l9 5a.75.75 0 0 0 .13.06h.1a1.06 1.06 0 0 0 .5 0h.1l.14-.06 9-5A1 1 0 0 0 22 16V8zm-10 3.87L5.06 8l2.76-1.52 6.83 3.9zm0-7.72L18.94 8 16.7 9.25 9.87 5.34zM4 9.7l7 3.92v5.68l-7-3.89zm9 9.6v-5.68l3-1.68V15l2-1v-3.18l2-1.11v5.7z">
-                            </path>
-                        </svg>
-                        Free delivery / Worldwide from $27</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            style="transform: ;msFilter:;">
-                            <path
-                                d="M22 8a.76.76 0 0 0 0-.21v-.08a.77.77 0 0 0-.07-.16.35.35 0 0 0-.05-.08l-.1-.13-.08-.06-.12-.09-9-5a1 1 0 0 0-1 0l-9 5-.09.07-.11.08a.41.41 0 0 0-.07.11.39.39 0 0 0-.08.1.59.59 0 0 0-.06.14.3.3 0 0 0 0 .1A.76.76 0 0 0 2 8v8a1 1 0 0 0 .52.87l9 5a.75.75 0 0 0 .13.06h.1a1.06 1.06 0 0 0 .5 0h.1l.14-.06 9-5A1 1 0 0 0 22 16V8zm-10 3.87L5.06 8l2.76-1.52 6.83 3.9zm0-7.72L18.94 8 16.7 9.25 9.87 5.34zM4 9.7l7 3.92v5.68l-7-3.89zm9 9.6v-5.68l3-1.68V15l2-1v-3.18l2-1.11v5.7z">
-                            </path>
-                        </svg>
-                        Free delivery / Worldwide from $27</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            style="transform: ;msFilter:;">
-                            <path
-                                d="M22 8a.76.76 0 0 0 0-.21v-.08a.77.77 0 0 0-.07-.16.35.35 0 0 0-.05-.08l-.1-.13-.08-.06-.12-.09-9-5a1 1 0 0 0-1 0l-9 5-.09.07-.11.08a.41.41 0 0 0-.07.11.39.39 0 0 0-.08.1.59.59 0 0 0-.06.14.3.3 0 0 0 0 .1A.76.76 0 0 0 2 8v8a1 1 0 0 0 .52.87l9 5a.75.75 0 0 0 .13.06h.1a1.06 1.06 0 0 0 .5 0h.1l.14-.06 9-5A1 1 0 0 0 22 16V8zm-10 3.87L5.06 8l2.76-1.52 6.83 3.9zm0-7.72L18.94 8 16.7 9.25 9.87 5.34zM4 9.7l7 3.92v5.68l-7-3.89zm9 9.6v-5.68l3-1.68V15l2-1v-3.18l2-1.11v5.7z">
-                            </path>
-                        </svg>
-                        Free delivery / Worldwide from $27</p>
-                    <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            style="transform: ;msFilter:;">
-                            <path
-                                d="M22 8a.76.76 0 0 0 0-.21v-.08a.77.77 0 0 0-.07-.16.35.35 0 0 0-.05-.08l-.1-.13-.08-.06-.12-.09-9-5a1 1 0 0 0-1 0l-9 5-.09.07-.11.08a.41.41 0 0 0-.07.11.39.39 0 0 0-.08.1.59.59 0 0 0-.06.14.3.3 0 0 0 0 .1A.76.76 0 0 0 2 8v8a1 1 0 0 0 .52.87l9 5a.75.75 0 0 0 .13.06h.1a1.06 1.06 0 0 0 .5 0h.1l.14-.06 9-5A1 1 0 0 0 22 16V8zm-10 3.87L5.06 8l2.76-1.52 6.83 3.9zm0-7.72L18.94 8 16.7 9.25 9.87 5.34zM4 9.7l7 3.92v5.68l-7-3.89zm9 9.6v-5.68l3-1.68V15l2-1v-3.18l2-1.11v5.7z">
-                            </path>
-                        </svg>
-                        Free delivery / Worldwide from $27</p>
-
-
-                </div>
+                <?php
+                if ($nav_boxes): ?>
+                    <div class="nav__bottom-boxes">
+                        <?php foreach ($nav_boxes as $box): 
+                      
+                            $text = $box['text'] ?? '';
+                            $img_id = $box['img'] ?? '';
+                            $img_url = $img_id ? wp_get_attachment_image_url($img_id, 'full') : ''; // Uzyskanie URL obrazka
+                            $img_alt = $img_id ? get_post_meta($img_id, '_wp_attachment_image_alt', true) : ''; // Tekst alternatywny
+                        ?>
+                            <div class="nav__bottom-box">
+                                <?php if ($img_url && $text): ?>
+                                    
+                                        <img src="<?php echo esc_url($img_url); ?>" alt="<?php echo esc_attr($img_alt); ?>">
+                                        <p><?php echo esc_html($text); ?></p>
+                                    
+                                <?php endif; ?>
+                                
+                                
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+               
             </div>
         </div>
     </div>
